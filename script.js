7602bc6ba3d9263ee8c2cee8a4e404f51e17dd5e
Qazi-Ghulam-Mustafa <Qazi-Ghulam-Mustafa>
2025-09-21 21:56:30 +0500

    Updated Commit

--- script.js
+++ script.js
@@ -1,37 +1,24 @@
-// var Maths = +prompt('enter Maths marks');
-// var Physics = +prompt('enter Physics marks');
-// var English = +prompt('enter English marks');
-// var Computer = +prompt('enter Computer marks');
-// var Urdu = +prompt('enter Urdu marks');
-// var obtainedMarks = Urdu + English + Maths + Physics + Computer;
-// alert(obtainedMarks)
-// var totalMarks = 500;
-// var percentage = (obtainedMarks / totalMarks) * 100;
-// alert(percentage)
+var Maths = +prompt('enter Maths marks');
+var Physics = +prompt('enter Physics marks');
+var English = +prompt('enter English marks');
+var Computer = +prompt('enter Computer marks');
+var Urdu = +prompt('enter Urdu marks');
+var obtainedMarks = Urdu + English + Maths + Physics + Computer;
+alert(obtainedMarks)
+var totalMarks = 500;
+var percentage = (obtainedMarks / totalMarks) * 100;
+alert(percentage)
 
-// if (percentage >= 90) {
-//     alert("you got A1 Grade in your exam")
-// } else if (percentage >= 80) {
-//     alert("you got A Grade in your exam")
-// } else if (percentage >= 70) {
-//     alert("you got B Grade in your exam")
-// } else if (percentage >= 60) {
-//     alert("you got C Grade in your exam ")
-// } else if (percentage >= 50) {
-//     alert("you got D Grade in your exam ")
-// } else if (percentage >= 40) {
-//     alert("you are Fail in your exam ")
-// }
-
-// var num= prompt("How much time i print table of 3");
-// for(index=1; index<21; index++ ){
-//     console.log(index*5)
-// }
-
-
-// this is Array and the index of this array is 6 and length of this array is 7 
-
-var continents = ["Asia", "Africa", "Europe", "Antartica", "North america", "South America", "Australia"];
-for(index=0 ; index < continents.length; index++ ){
-    console.log(continents[index])
+if (percentage >= 90) {
+    alert("you got A1 Grade in your exam")
+} else if (percentage >= 80) {
+    alert("you got A Grade in your exam")
+} else if (percentage >= 70) {
+    alert("you got B Grade in your exam")
+} else if (percentage >= 60) {
+    alert("you got C Grade in your exam ")
+} else if (percentage >= 50) {
+    alert("you got D Grade in your exam ")
+} else if (percentage >= 40) {
+    alert("you are Fail in your exam ")
 }
